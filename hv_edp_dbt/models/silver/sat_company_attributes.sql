d98daec078aabb9b326ea185d45e34bec9233331
Polina Anisova <panisova@harbourvest.com>
2025-11-14 17:20:42 -0500

    Rename attributes

--- hv_edp_dbt/models/silver/sat_company_attributes.sql
+++ hv_edp_dbt/models/silver/sat_company_attributes.sql
@@ -4,12 +4,12 @@ with attributes as (
         company_broad.short_name as company_name,
         currency.name as company_currency,
         CASE WHEN company_broad.is_public = 1 THEN 'PUBLIC' ELSE 'PRIVATE' END as company_status,
-        company.business_desc as company_exposure_business_description,
-        dim_geo.L1_description as company_exposure_geography_broad,
-        dim_geo.L3_description as company_exposure_geography_country,
-        company_hier.L1_description as company_exposure_industry_category,
-        company_hier.L2_description as company_exposure_industry_broad,
-        company_hier.L3_description as company_exposure_industry,
+        company.business_desc as company_business_description,
+        dim_geo.L1_description as company_geography_broad,
+        dim_geo.L3_description as company_geography_country,
+        company_hier.L1_description as company_industry_category,
+        company_hier.L2_description as company_industry_broad,
+        company_hier.L3_description as company_industry,
         CURRENT_TIMESTAMP() as load_dt
     FROM
         {{ source('bronze_from_harborview_edw', 'company') }} company
@@ -28,11 +28,11 @@ SELECT
 company_name,
 company_currency,
 company_status,
-company_exposure_business_description,
-company_exposure_geography_broad,
-company_exposure_geography_country,
-company_exposure_industry_category,
-company_exposure_industry_broad,
-company_exposure_industry,
+company_business_description,
+company_geography_broad,
+company_geography_country,
+company_industry_category,
+company_industry_broad,
+company_industry,
 {{ encoded_hashed_row() }} as skey
 FROM attributes
