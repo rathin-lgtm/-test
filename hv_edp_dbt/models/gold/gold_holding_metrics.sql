6f10930eda0af899bf9c1d02a50c7516bdcfc28f
Andre Di Genova <adigenova@harbourvest.com>
2025-12-17 14:54:55 -0500

    Add disclosure level to holding attributes.

--- hv_edp_dbt/models/gold/gold_holding_metrics.sql
+++ hv_edp_dbt/models/gold/gold_holding_metrics.sql
@@ -3,7 +3,8 @@ SELECT
     as_of_date,
     holding_id,
     holding_currency,
-    holding_commitment_unlevered
+    holding_commitment_unlevered,
+    holding_disclosure_level
 FROM {{ ref('sat_holding_metrics') }} m
 JOIN {{ ref('sat_holding_attributes') }} a
     ON m.hk_holding = a.hk_holding
--- hv_edp_dbt/models/silver/sat_holding_attributes.sql
+++ hv_edp_dbt/models/silver/sat_holding_attributes.sql
@@ -6,7 +6,8 @@ with attributes as (
         CURRENT_TIMESTAMP() as load_dt,
         holding.start_eff_date,
         holding.end_eff_date,
-        holding.active_ind
+        holding.active_ind,
+        holding.holding_disclosure_level
     FROM
         {{ source('bronze_from_harborview_edw', 'dim_holdings') }} holding
     JOIN {{ source('bronze_from_harborview_edw', 'currency') }} currency
@@ -20,6 +21,7 @@ final_attributes as (
     SELECT 
         hk_holding,
         holding_currency,
+        holding_disclosure_level,
         fund_id,
         load_dt
     FROM attributes
